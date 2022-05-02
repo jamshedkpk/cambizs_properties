e8c61d076ef489ad9f75755dc88b39ea789b8ce4
jamshedkpk <jamshedkpk@yahoo.com>
2022-05-02 15:16:47 +0500

    Updated

---
+++ resources/views/admin/layout/dashboard.blade.php
@@ -0,0 +1,4 @@
+@extends('admin.layout.app')
+@section('content')
+<h3>This is Admin Home Page</h3>
+@endsection
---
+++ resources/views/user/layout/dashboard.blade.php
@@ -0,0 +1,4 @@
+@extends('user.layout.app')
+@section('content')
+<h3>This is user Home Page</h3>
+@endsection
